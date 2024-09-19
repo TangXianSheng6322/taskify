c5bc9996da6f55d2b3b2358385a47affeb6ac91d
Nikita Sakharov <nick@Nikitas-MacBook-Air.local>
2024-09-19 22:18:52 +0700

    Add components

---
+++ src/components/InputField.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./InputField.css";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+  return (
+    <form className="input">
+      <input
+        type="input"
+        placeholder="Enter a Task"
+        className="input_box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button className="input_submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
