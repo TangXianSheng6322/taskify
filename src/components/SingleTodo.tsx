58b85d31f063e4734f3f412eccf1c81ef5e701c4
Nikita Sakharov <nick@Nikitas-MacBook-Air.local>
2024-09-21 01:25:35 +0700

    New Copy Of Taskify

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useState } from "react";
 import { FaEdit, FaCheck } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
 import { Todo } from "../model";
+import { useRef } from "react";
+import { useEffect } from "react";
 import "./styles.css";
 
 type Props = {
@@ -11,18 +14,67 @@ type Props = {
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    // inputRef.current?.focus();
+    inputRef.current?.select();
+  }, [edit]);
+
   return (
-    <form className="todos_single">
+    <form className="todos_single" onSubmit={(e) => handleEdit(e, todo.id)}>
       {" "}
-      <span className="todos_single--text">{todo.todo}</span>{" "}
+      {edit ? (
+        <input
+          ref={inputRef}
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="todos_single--text"
+        />
+      ) : todo.isDone ? (
+        <s className="todos_single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos_single--text">{todo.todo}</span>
+      )}
       <div>
-        <span className="icon">
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
           <FaEdit />
         </span>
         <span className="icon">
-          <FaCheck />
+          <FaCheck onClick={() => handleDone(todo.id)} />
         </span>
-        <span className="icon">
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
           <MdDelete />
         </span>
       </div>
